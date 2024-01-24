d00d8c680310ea473a2cca1af84455743a8096a6
ducanh <ducanha2k46@gmail.com>
2024-01-24 22:00:45 +0700

    change rgcn

--- module/rgcn.py
+++ module/rgcn.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import numpy as np
 # import dgl
 from torch_geometric.nn import RGCNConv
-from transformers import BertModel
 
 def make_graph(speakers:torch.Tensor, device):
     edge_index, edge_type = [], []
@@ -11,8 +10,17 @@ def make_graph(speakers:torch.Tensor, device):
     batch_size = speakers.size(0)
     total_len = 0
     for j in range(batch_size):
-        for l in range(length):
-            for k in range(length):
+        length_max = 0
+        for i in range(length - 1):
+            print(speakers[j, i], speakers[j, i+1])
+            if (speakers[j, i] == speakers[j, i+1]):
+                length_max = i + 2
+                break
+        if (length_max == 0):
+            length_max = length
+        print(length_max)
+        for l in range(length_max):
+            for k in range(length_max):
                 edge_index.append(torch.tensor([l + total_len, k + total_len]))
                 if (speakers[j, l] == speakers[j, k]):
                     edge_type.append(1)
@@ -38,23 +46,20 @@ class RGCN(nn.Module):
 if __name__ == "__main__":
     device = "cpu"
     batch = torch.randn((15, 5))
-    speakers = torch.tensor([[0, 1, 1, 0, 0], [0, 1, 0, 1, 1], [0, 1, 0, 1, 0]])
-
+    speakers = torch.tensor([[0, 1, 0, 1, 1], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0]])
     edge_index, edge_type = make_graph(speakers, device)
-
     conv = RGCN(5, 5)
-    bert = BertModel.from_pretrained('bert-base-cased')
-    emotion_linear = nn.Linear(5, 7)
     out_feature = conv(batch,edge_index, edge_type)
-    for i in range(out_feature.shape[0]):
-        node_feature = out_feature[i]
-        # print(node_feature.shape)
-        # print(node_feature)
-        print(emotion_linear(node_feature))
-        # Tiến hành xử lý với node_feature tại đây
+    # for i in range(out_feature.shape[0]):
+    #     node_feature = out_feature[i]
+    #     # print(node_feature.shape)
+    #     # print(node_feature)
+    #     print(emotion_linear(node_feature))
+    #     # Tiến hành xử lý với node_feature tại đây
 
-    print(emotion_linear(out_feature))
+    # print(emotion_linear(out_feature))
 
-
-
-
+    print(edge_index.shape)
+    print(edge_index)
+    print(edge_type.shape)
+    print(edge_type)
